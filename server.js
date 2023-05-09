ab88499d7cde062c0f0bfac34b5768def563078a
Thaphat-M <cryboyfeeling@outlook.com>
2023-05-09 20:51:09 +0700

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var cors = require('cors')
 require('dotenv').config()
-const mysql = require('mysql2');
 
+const mysql = require('mysql2')
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 
 var app = express()
