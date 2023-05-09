effefdeabf581e2d44e7cbe999be27e56ed20ebf
Thaphat-M <cryboyfeeling@outlook.com>
2023-05-09 21:08:46 +0700

    update employee

--- server.js
+++ server.js
@@ -29,6 +29,45 @@ app.get('/employee/:id', function (req, res, next) {
   );
 })
 
+app.post('/employee', function (req, res, next) {
+  connection.query(
+    'INSERT INTO `employee`(`fname`, `lname`, `idcard`, `username`, `password`, `email`, `avatar`) VALUES (?, ?, ?, ?, ?, ?, ?)',
+    [req.body.fname, req.body.lname, req.body.idcard, req.body.username, req.body.password, req.body.email, req.body.avatar],
+    function(err, results) {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to add employee' });
+        return;
+      }
+      res.json(results);
+    }
+  );
+});
+
+app.put('/employee/:id', function (req, res, next) {
+  connection.query(
+    'UPDATE `employee` SET `fname`= ?, `lname`= ?, `idcard`= ?, `username`= ?, `password`= ?, `email`= ?, `avatar`= ? WHERE id = ?',
+    [req.body.fname, req.body.lname, req.body.idcard, req.body.username, req.body.password, req.body.email, req.body.avatar, req.params.id],
+    function(err, results) {
+      if (err) {
+        res.status(500).json(err);
+      } else {
+        res.json(results);
+      }
+    }
+  );
+});
+
+app.delete('/employee', function (req, res, next) {
+  connection.query(
+    'DELETE FROM `employee` WHERE id = ?',
+    [req.body.id],
+    function(err, results) {
+      res.json(results);
+    }
+  );
+})
+
 app.listen(5000, function () {
   console.log('CORS-enabled web server listening on port 5000')
 })
